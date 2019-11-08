f27484a6b83a8113353648e19e65afbdd3e57098
khoo teck wei <kennyzai99@hotmail.com>
2019-11-08 19:08:42 +0800

    kiki

--- app/src/main/java/com/example/colourview/MainActivity.kt
+++ app/src/main/java/com/example/colourview/MainActivity.kt
@@ -14,17 +14,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setListeners()
 
+
     }
 
     private fun makeColored(view: View) {
         when (view.id) {
 
             // Boxes using Color class colors for the background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            /*R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)*/
+
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.miku1)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.miku2)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.miku3)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.miku4)
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.miku1)
+
 
             // Boxes using custom colors for background
             R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
@@ -58,6 +66,7 @@ class MainActivity : AppCompatActivity() {
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
         }
+
 
     }
 }
